0af1679a3c691fcb9766f4d1ac73d6d5b275afe7
Gabo Rohez <gabow95k@gmail.com>
2022-09-21 14:45:01 -0500

    add textToSpeech using edittext to get message

--- app/src/main/java/gaborohez/texttospeach/MainActivity.kt
+++ app/src/main/java/gaborohez/texttospeach/MainActivity.kt
@@ -1,11 +1,66 @@
 package gaborohez.texttospeach
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import gaborohez.texttospeach.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    private lateinit var binding: ActivityMainBinding
+
+    var textToSpeech: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (!isEnableSpeech()) {
+            showDialog()
+        }
+
+        setUpEvents()
+    }
+
+    private fun setUpEvents() {
+        binding.btnPlay.setOnClickListener {
+            val messageToSpeech = binding.tilMessage.editText!!.text.toString().trim()
+            textToSpeech!!.speak(messageToSpeech, TextToSpeech.QUEUE_FLUSH, null, "")
+        }
+    }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(getString(R.string.error_message))
+            .setPositiveButton(
+                getString(R.string.accept)
+            ) { dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+        builder.create()
+        builder.show()
+    }
+
+    private fun isEnableSpeech(): Boolean {
+        var flag = true
+        textToSpeech = TextToSpeech(this) {
+            if (it == TextToSpeech.SUCCESS) {
+                flag = true
+                Log.d(TAG, getString(R.string.success))
+                textToSpeech!!.language = Locale.US
+            } else {
+                flag = false
+                Log.d(TAG, getString(R.string.no_available))
+            }
+        }
+        return flag
     }
 }
